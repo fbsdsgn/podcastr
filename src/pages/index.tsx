e02cf55159c6e8925acccec4aca1313efa6adec9
Felipe B. Souza <fbsdsgn@gmail.com>
2022-08-22 15:58:31 -0300

    Adding api request and consumption to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,96 @@
-export default function Home() {
-  return <span>Index</span>;
+import { GetStaticProps } from "next";
+
+import ptBR from "date-fns/locale/pt-BR";
+import { format, parseISO } from "date-fns";
+
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+
+import styles from "./home.module.scss";
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  description: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  publishedAt: string;
+};
+
+type HomeProps = {
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
+};
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  return (
+    <div className={styles.home}>
+      <section className={styles.home__episodes}>
+        <h2>Últimos Lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map((episode) => {
+            return (
+              <li key={episode.id}>
+                <img src={episode.thumbnail} alt={episode.title} />
+                <div className={styles.home__episodesDetails}>
+                  <a href="#">{episode.title}</a>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+
+                <button type="button">
+                  <img src="/assets/svg/play-green.svg" alt="Tocar episódio" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className={styles.home__episodesAll}></section>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      durationAsString: convertDurationToTimeString(
+        Number(episode.file.duration)
+      ),
+      duration: Number(episode.file.duration),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
